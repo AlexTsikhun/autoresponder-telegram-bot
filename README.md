685f9b7f9a2960902a81c3b8b21d421ea79c6880
Alexander <sasacihun4@gmail.com>
2023-05-04 14:21:09 +0300

    Avoiding spam, update readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-"# autoresponder-telegram-bot" 
+# autoresponder-telegram-bot
+
+## Development of an answering machine for telegrams
+Task from: Royal Techno
+
+General description of the task:
+
+Development of a neural network language bot of telegrams
+Language: Python
+1. Sending messages by user base
+2. Mailing to 1000 users in one session
+DO NOT SPAM
+3. "autoresponder" function
+
+### Project Structure:
+```
+├───README.md                  <- The top-level README for developers using this project
+|
+├───.gitignore                 <- Ignore files
+|
+├───Dockerfile                 <- Assemble an image
+|                             
+├───docker-compose.yml         <- Defining services
+|
+├───telegram_db                <- SQLite database
+|
+├───db.py                      <- Database queries
+├───main.py                    <- Bot
+|
+|
+└───requirements.txt           <- The requirements file for reproducing the analysis environment, e.g. generated with `pip freeze > requirements.txt`
+```
+
+### Solution
+Telegram-bot, that you can chat with - this function provides OpenAI API (CHATGPT). Writtern in python library aiogram Bot, which contain information about users in database SQLite (telegram_db). 
+
+Admin can send messages to 1000 users with command:
+
+```
+/send2all YOUR_MESSAGE
+```
+
+Program wrap in docker container for deploing. 
+
--- main.py
+++ main.py
@@ -1,60 +1,66 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-# import class
 from db import Database
 import openai
 import os
+import time
 from dotenv import load_dotenv, find_dotenv
 
 logging.basicConfig(level=logging.INFO)
 
 # Find .evn file
 load_dotenv(find_dotenv())
-bot = Bot(os.getenv('TOKEN'))
-openai.api_key = os.getenv('OPENAI_TOKEN')
+bot = Bot(os.getenv("TOKEN"))
+openai.api_key = os.getenv("OPENAI_TOKEN")
 
 dp = Dispatcher(bot)
 db = Database("telegram_db")
 
-@dp.message_handler(commands=['start'])
-async def start(message:types.Message):
-    if message.chat.type == 'private':
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    if message.chat.type == "private":
         if not db.user_exists(message.from_user.id):
             db.add_user(message.from_user.id)
         await bot.send_message(message.from_user.id, "Hi!")
 
-@dp.message_handler(commands=['send2all'])
-async def send2all(message:types.Message):
-    if message.chat.type == 'private':
+
+@dp.message_handler(commands=["send2all"])
+async def send2all(message: types.Message):
+    if message.chat.type == "private":
         if message.from_user.id == 642700014:
-            # get message text
+            # Get message text
             text = message.text[10:]
             users = db.get_users()
+            # Send to 1000 users (include admin)
             for row in users[:1000]:
                 try:
                     await bot.send_message(row[0], text)
-                    # check active or not and if user get message - set him active
-                    if int(row[1])!=1:
+                    # Check active or not and if user get message - set him active
+                    if int(row[1]) != 1:
                         db.set_active(row[0], 1)
+                    # Avoid spam
+                    # Bot able send to 30 messages per sec, for this case .sleep(0.033)
+                    time.sleep(0.2)
                 except:
                     db.set_active(row[0], 0)
-            await bot.send_message(message.from_user.id, '!')
+            await bot.send_message(message.from_user.id, "!")
 
 
 @dp.message_handler()
-async def openai_response(message:types.Message):
+async def openai_response(message: types.Message):
     response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=message.text,
-    temperature=0.9,
-    max_tokens=1024,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"],
     )
     await message.answer(response.choices[0].text)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
